7412306e15b377d2c85dc8562817d9e3aef76351
lance <lanceparker2001@gmail.com>
2023-07-24 22:10:44 +0530

    Set sameSite to lax since frontend is in localhost

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -5,7 +5,7 @@ export const sendToken = (res, user, message, statusCode = 200) => {
     expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 15),
     httpOnly: true,
     secure: true, //true hone se website pe cookie ayegi postman pe nahi
-    sameSite: "none",
+    sameSite: "lax", //lax for dev(frontend localhost), // none when both hosted
   };
 
   res.status(201).cookie("token", token, options).json({
